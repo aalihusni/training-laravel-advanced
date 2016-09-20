059014df6ef8c8047840956062281087a57e0f2a
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-09-20 08:16:09 +0800

    remove closure artisan

--- routes/console.php
+++ routes/console.php
@@ -21,23 +21,23 @@ Artisan::command('test', function(){
 	$this->info('test');
 })->describe('some test');
 
-Artisan::command('clean:up', function(){
-	$this->call('view:clear');
+// Artisan::command('clean:up', function(){
+// 	$this->call('view:clear');
 
-	$this->call('config:clear');
-    $this->call('config:cache');
+// 	$this->call('config:clear');
+//     $this->call('config:cache');
     
-    $this->call('route:clear');
+//     $this->call('route:clear');
    
-    $this->call('optimize');
-})->describe('Clean up cache files');
+//     $this->call('optimize');
+// })->describe('Clean up cache files');
 
 
-Artisan::command('clean:serve {--port=8000}', function(){
-	$this->call('clean:up');
+// Artisan::command('clean:serve {--port=8000}', function(){
+// 	$this->call('clean:up');
 
-    $this->call('serve',['--port' => $this->option('port')]);
-})->describe('Clean up cache files and serve the application');
+//     $this->call('serve',['--port' => $this->option('port')]);
+// })->describe('Clean up cache files and serve the application');
 
 
 Artisan::command('ask',function(){
